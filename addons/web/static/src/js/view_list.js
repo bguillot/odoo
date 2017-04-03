70031f2ff528f35019b1c8dbaa0ed48dce937608
Benoit <benoit.guillot@akretion.com.br>
2017-04-03 12:24:01 +0200

    [IMP] web custom for enquete

--- addons/web/static/src/js/view_list.js
+++ addons/web/static/src/js/view_list.js
@@ -422,7 +422,7 @@ instance.web.ListView = instance.web.View.extend( /** @lends instance.web.ListVi
             if (range_stop > total) {
                 range_stop = total;
             }
-            spager = _.str.sprintf(_t("%d-%d of %d"), range_start, range_stop, total);
+            spager = _.str.sprintf(_t("Resultat : %d"), total);
         }
 
         this.$pager.find('.oe_list_pager_state').text(spager);
